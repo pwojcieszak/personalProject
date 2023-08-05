4dc365bc1f81a4ca949700c6eb524e1da7f7d02c
pwojcieszak <piotrekw1002@gmail.com>
2023-08-05 23:45:39 +0200

    Added controller with endpoint to simple front-end main page

---
+++ frontService/src/main/java/com/pwojcieszak/frontService/Controller/frontServiceController.java
@@ -0,0 +1,12 @@
+package com.pwojcieszak.frontService.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class frontServiceController {
+    @GetMapping("/")
+    public String getMainPage(){
+        return "index";
+    }
+}
