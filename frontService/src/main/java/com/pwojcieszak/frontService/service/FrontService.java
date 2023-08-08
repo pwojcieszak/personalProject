25e35b0f85cc47e8a402cd1842126b94b1ab1ed1
pwojcieszak <piotrekw1002@gmail.com>
2023-08-08 23:08:59 +0200

    Used WebClient to retrieve needed data from "skill-service"

---
+++ frontService/src/main/java/com/pwojcieszak/frontService/service/FrontService.java
@@ -0,0 +1,29 @@
+package com.pwojcieszak.frontService.service;
+
+import com.pwojcieszak.frontService.dto.SkillsResponse;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class FrontService {
+    private final WebClient webClient;
+
+    public FrontService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("http://localhost:8081").build();
+    }
+
+    public List<SkillsResponse> getAllSkills() {
+        Mono<List<SkillsResponse>> skillsMono = webClient.get()
+                .uri("/skills/all")
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<SkillsResponse>>() {});
+
+        return skillsMono.blockOptional()
+                .orElse(Collections.emptyList());
+    }
+}
