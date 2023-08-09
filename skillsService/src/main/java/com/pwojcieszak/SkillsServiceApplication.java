f2a8679dc2d18b6b37965db0966b0f78728bf025
pwojcieszak <piotrekw1002@gmail.com>
2023-08-10 00:57:56 +0200

    Added Eureka client annotations and changed port in skillsService and deleted unnecessary property in FrontService

--- skillsService/src/main/java/com/pwojcieszak/SkillsServiceApplication.java
+++ skillsService/src/main/java/com/pwojcieszak/SkillsServiceApplication.java
@@ -2,10 +2,12 @@ package com.pwojcieszak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import javax.swing.*;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SkillsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkillsServiceApplication.class, args);
