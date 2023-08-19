98f0061ee1a5bee491cb103b841b60085f5dada7
pwojcieszak <piotrekw1002@gmail.com>
2023-08-19 15:01:40 +0200

    Configured Security to use converter from keycloak roles to spring roles. Implemented that converter

--- frontService/src/main/java/com/pwojcieszak/frontService/security/SecurityConfig.java
+++ frontService/src/main/java/com/pwojcieszak/frontService/security/SecurityConfig.java
@@ -1,8 +1,9 @@
 package com.pwojcieszak.frontService.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,23 +13,30 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthConverter jwtAuthConverter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .anyRequest().authenticated());
+                        .anyRequest().authenticated()
+                );
 
         http
-                .oauth2ResourceServer(
-                        server -> server
-                                .jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(server -> server
+                                .jwt(jwtConfigurer -> jwtConfigurer
+                                        .jwtAuthenticationConverter(jwtAuthConverter)
+                                )
+                );
 
 
         http
                 .sessionManagement(customizer -> customizer
-                        .sessionCreationPolicy(STATELESS));
+                        .sessionCreationPolicy(STATELESS)
+                );
 
         return http.build();
     }
