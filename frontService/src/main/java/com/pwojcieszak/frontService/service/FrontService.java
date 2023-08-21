a8eb0524c9f5964711a410139a2da61129a1a8a4
pwojcieszak <piotrekw1002@gmail.com>
2023-08-22 00:27:44 +0200

    Added custom tracer used for signalizing call to other service

--- frontService/src/main/java/com/pwojcieszak/frontService/service/FrontService.java
+++ frontService/src/main/java/com/pwojcieszak/frontService/service/FrontService.java
@@ -2,6 +2,9 @@ package com.pwojcieszak.frontService.service;
 
 import com.pwojcieszak.frontService.dto.SkillsRequest;
 import com.pwojcieszak.frontService.dto.SkillsResponse;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -13,45 +16,64 @@ import java.util.Collections;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class FrontService {
+    private final Tracer tracer;
     private final WebClient.Builder webClientBuilder;
 
-    public FrontService(WebClient.Builder webClientBuilder) {
-        this.webClientBuilder = webClientBuilder;
-    }
-
-
     public List<SkillsResponse> getAllSkills() {
-        Mono<List<SkillsResponse>> skillsMono = webClientBuilder.build().get()
-                .uri("http://SKILLS-SERVICE/api/skills/all")
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<SkillsResponse>>() {})
-                .onErrorResume(throwable -> Mono.just(Collections.emptyList()));
+        Span skillsServiceCall = tracer.nextSpan().name("SkillsServiceCall");
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(skillsServiceCall.start())) {
+            Mono<List<SkillsResponse>> skillsMono = webClientBuilder.build().get()
+                    .uri("http://SKILLS-SERVICE/api/skills/all")
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<List<SkillsResponse>>() {
+                    })
+                    .onErrorResume(throwable -> Mono.just(Collections.emptyList()));
 
-        return skillsMono.blockOptional()
-                .orElse(Collections.emptyList());
+            return skillsMono.blockOptional()
+                    .orElse(Collections.emptyList());
+        }
+        finally {
+            skillsServiceCall.end();
+        }
     }
 
     public Boolean deleteSkill(String name) {
-        Mono<Boolean> skillsMono = webClientBuilder.build().delete()
-                .uri("http://SKILLS-SERVICE/api/skills/delete/{name}", name)
-                .retrieve()
-                .toBodilessEntity()
-                .map(responseEntity -> responseEntity.getStatusCode() == HttpStatus.OK)
-                .defaultIfEmpty(false);
+        Span skillsServiceCall = tracer.nextSpan().name("SkillsServiceCall");
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(skillsServiceCall.start())){
+            Mono<Boolean> skillsMono = webClientBuilder.build().delete()
+                    .uri("http://SKILLS-SERVICE/api/skills/delete/{name}", name)
+                    .retrieve()
+                    .toBodilessEntity()
+                    .map(responseEntity -> responseEntity.getStatusCode() == HttpStatus.OK)
+                    .defaultIfEmpty(false);
 
-        return skillsMono.blockOptional()
-                        .orElse(false);
+            return skillsMono.blockOptional()
+                    .orElse(false);
+        }
+        finally {
+            skillsServiceCall.end();
+        }
     }
 
     public boolean createSkill(SkillsRequest skillsRequest) {
-        Mono<Boolean> skillsMono = webClientBuilder.build().post()
-                .uri("http://SKILLS-SERVICE/api/skills/new")
-                .body(BodyInserters.fromValue(skillsRequest))
-                .retrieve()
-                .bodyToMono(Boolean.class)
-                .onErrorResume(throwable -> Mono.just(false));
-        return skillsMono.blockOptional()
-                .orElse(false);
+        Span skillsServiceCall = tracer.nextSpan().name("SkillsServiceCall");
+
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(skillsServiceCall.start())){
+            Mono<Boolean> skillsMono = webClientBuilder.build().post()
+                    .uri("http://SKILLS-SERVICE/api/skills/new")
+                    .body(BodyInserters.fromValue(skillsRequest))
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .onErrorResume(throwable -> Mono.just(false));
+            return skillsMono.blockOptional()
+                    .orElse(false);
+        }
+        finally {
+            skillsServiceCall.end();
+        }
+
+
     }
 }
